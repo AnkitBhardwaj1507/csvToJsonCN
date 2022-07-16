ed74bbf72b0052419aa8d02a3d888ba78270855f
Ankit <bhardwaj.ankit31@gmail.com>
2022-07-16 20:29:56 +0530

    final changes

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const bodyParser = require("body-parser");
-// const csv = require('csv-parser');
+
 const app = express();
 
 const port = 8000;
 const db = require("./config/mongoose");
 
-// app.use(bodyParser.json());
 app.use(express.urlencoded());
 
 app.use(express.static("./assets"));
